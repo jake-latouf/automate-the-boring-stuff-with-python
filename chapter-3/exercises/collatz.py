986dbf39a5d9ef42553b7ecccbc50ad1eee1469e
Jake LaTouf <jakeslatouf@gmail.com>
2024-05-14 15:46:47 -0500

    Finished chapter 3 exercises

---
+++ chapter-3/exercises/collatz.py
@@ -0,0 +1,15 @@
+def collatz(number):
+    if number % 2 == 0:
+        return number // 2
+    elif number % 2 == 1:
+        return 3 * number + 1
+
+while True:
+    print("Enter number:")
+    number = input()
+    try:
+        number = int(number)
+        while number > 1:
+          collatz(number)
+    except ValueError:
+        print("Please enter a valid integer.")
